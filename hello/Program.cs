339c7a28cc13055218187da6d57836b02f8a0ba2
Jonas <jonasper19@gmail.com>
2025-06-02 17:12:08 -0300

    first commit

---
+++ hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Hello
+{
+	static void DrawText(string text, string? text2 = null)
+	{
+		if (!string.IsNullOrEmpty(text2))
+			Console.WriteLine($"My first text: {text}\nMy second text: {text2}");
+		else
+			Console.WriteLine($"I got only: {text}");
+	}
+	static void Main()
+	{
+		DrawText("Hello world", "I'm learning c#");
+		DrawText("Say hi", null);
+	}
+}
+
+
---
+++ lala.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+# define ORANGE "\033[38;5;208m"
+# define RESET "\033[0m"
+
+static void	drawTitle(const std::string &title, bool colour = false)
+{
+	if (colour)
+		std::cout << ORANGE;
+	std::cout << "\t\t" << title << RESET << std::endl;
+}
+
+int main(void)
+{
+	drawTitle("Titulo laranja", 1);
+	drawTitle("Titulo sem cor");
+	return (0);
+}
